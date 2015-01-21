d39c482f9bc975b74ac1828677bd23ca24ad1396
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2015-01-21 15:35:22 +0100

    Use Barometer msg in publisher and add Barometer publisher node to launch file

--- ravros/scripts/barometer.py
+++ ravros/scripts/barometer.py
@@ -4,31 +4,27 @@
 #
 # ROS node example that publishes barometer data to a ROS topic
 
+import argparse
 import rospy
 import ravlib.sensors as sensors
-import std_msgs.msg as std_msgs
+import rav_msgs.msg as rav_msgs
 
-def talker():
+def talker(barometer_addr, update_rate):
     # Initialise sensor
     LPS331_I2C_ADDRESS = 0x5c
-    barometer = sensors.Barometer(address=LPS331_I2C_ADDRESS)
+    barometer = sensors.Barometer(address=barometer_addr)
     barometer.display_barometer()
     print "Barometer: Check."
 
     # Initialise publisher
     barometer_pub = rospy.Publisher('barometer', queue_size=1)
     rospy.init_node('barometer_stream', anonymous=True)
-    rate = rospy.Rate(2) # 2hz
+    rate = rospy.Rate(update_rate)
 
     # Infinite loop of reading & publishing
     while not rospy.is_shutdown():
         # Initialise message structure
-        barometer_msg.header = std_msgs.Header()
-        barometer_msg.pressure = std_msgs.Float64()
-        barometer_msg.pressure_variance = std_msgs.Float64()
-        barometer_msg.altitude = std_msgs.Float64()
-        barometer_msg.temperature = std_msgs.Float64()
-        barometer_msg.temperature_variance = std_msgs.Float64()
+        barometer_msg = rav_msgs.Barometer()
 
         # Read barometer
         pressure, altitude, temperature = barometer.get_data()
@@ -41,7 +37,7 @@ def talker():
         # Write sensor data: pressure in Pa, altitude in m, temperature in C
         barometer_msg.pressure = pressure
         barometer_msg.pressure_variance = -1 # No info
-        barometer_msg.altitude = altitude
+        barometer_msg.altitude_estimate = altitude
         barometer_msg.temperature = temperature
         barometer_msg.temperature_variance = -1 # No info
 
@@ -50,8 +46,20 @@ def talker():
         rate.sleep()
 
 if __name__ == '__main__':
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='ROS node that publishes \
+                                     Barometer data to a ROS topic')
+    parser.add_argument('barometer_addr', metavar='addr', type=str, \
+                        default='0x5c', help='Barometer address on I2C bus')
+    parser.add_argument('update_rate', metavar='rate', type=int, default=100, \
+                        help='Update rate (Hz)')
+    args, unknown = parser.parse_known_args()
+    barometer_addr = int(args.barometer_addr, 0) # Get hex from string
+    update_rate = args.update_rate
+
+    # Launch publisher
     try:
-        talker()
+        talker(barometer_addr, update_rate)
     except rospy.ROSInterruptException:
         pass
 
