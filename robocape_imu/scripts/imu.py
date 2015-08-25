aa7b1c41015c91c84c379fdbceda1e954878d8e8
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2015-08-25 18:08:01 +0200

    Refactor message packing - IMU

--- robocape_imu/scripts/imu.py
+++ robocape_imu/scripts/imu.py
@@ -273,49 +273,56 @@ class ImuHandler:
         self.pub = rospy.Publisher('imu_readings', Imu, queue_size=queue_size)
         self.rate = rospy.Rate(rospy.get_param('imu/rate', 10))
 
+        self.msg = self.msg_template()
+
         while not rospy.is_shutdown():
             rospy.loginfo(rospy.get_caller_id() + "publishing IMU data")
 
             self.imu.read()
-            msg = self.pack_message()
-            self.pub.publish(msg)
+            self.pack_message()
+            self.pub.publish(self.msg)
 
             self.rate.sleep()
 
-    def pack_message(self):
+    def msg_template(self):
         msg = Imu()
-
-        now = rospy.get_rostime()
-        msg.header.stamp.secs = now.secs
-        msg.header.stamp.nsecs = now.nsecs
         msg.header.frame_id = "robocape"
 
-        msg.linear_acceleration.x = self.imu.accel[0]
-        msg.linear_acceleration.y = self.imu.accel[1]
-        msg.linear_acceleration.z = self.imu.accel[2]
         msg.linear_acceleration_covariance = (0.1, 0.0, 0.0,
                                               0.0, 0.1, 0.0,
                                               0.0, 0.0, 0.1)
 
-        msg.angular_velocity.x = self.imu.gyro[0]
-        msg.angular_velocity.y = self.imu.gyro[1]
-        msg.angular_velocity.z = self.imu.gyro[2]
         msg.angular_velocity_covariance = (1.0, 0.0, 0.0,
                                            0.0, 1.0, 0.0,
                                            0.0, 0.0, 1.0)
 
-        quaternion = tf.transformations.quaternion_from_euler(self.imu.compass[0],
-                                                              self.imu.compass[1],
-                                                              self.imu.compass[2])
-        msg.orientation.x = quaternion[0]
-        msg.orientation.y = quaternion[1]
-        msg.orientation.z = quaternion[2]
-        msg.orientation.w = quaternion[3]
         msg.orientation_covariance = (10.0, 0.0,  0.0,
                                       0.0,  10.0, 0.0,
                                       0.0,  0.0,  10.0)
 
         return msg
+
+    def pack_message(self):
+        now = rospy.get_rostime()
+        self.msg.header.stamp.secs = now.secs
+        self.msg.header.stamp.nsecs = now.nsecs
+
+        self.msg.linear_acceleration.x = self.imu.accel[0]
+        self.msg.linear_acceleration.y = self.imu.accel[1]
+        self.msg.linear_acceleration.z = self.imu.accel[2]
+
+        self.msg.angular_velocity.x = self.imu.gyro[0]
+        self.msg.angular_velocity.y = self.imu.gyro[1]
+        self.msg.angular_velocity.z = self.imu.gyro[2]
+
+        quaternion = tf.transformations.quaternion_from_euler(self.imu.compass[0],
+                                                              self.imu.compass[1],
+                                                              self.imu.compass[2])
+        self.msg.orientation.x = quaternion[0]
+        self.msg.orientation.y = quaternion[1]
+        self.msg.orientation.z = quaternion[2]
+        self.msg.orientation.w = quaternion[3]
+
 
 if __name__ == '__main__':
     try:
