2c23ee3a469d7453755a5c915f3a324c44885105
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2015-08-25 14:07:08 +0200

    ROS publisher - IMU

--- robocape_imu/scripts/imu.py
+++ robocape_imu/scripts/imu.py
@@ -1,5 +1,9 @@
-import mraa
+from __future__ import division
 import math
+import rospy
+import tf
+from sensor_msgs.msg import Imu
+import mraa
 
 ### MPU 9150 Register Map
 
@@ -248,3 +252,70 @@ class Mpu9150:
         'Returns compass measurement normalised'
         norm = (self.compass[0]**2 + self.compass[1]**2 + self.compass[2]**2)**0.5
         return self.compass[0] / norm, self.compass[1] / norm, self.compass[2] / norm
+
+
+class ImuHandler:
+    def __init__(self):
+        node_name = 'imu_mpu9150_handler'
+        rospy.init_node(node_name)
+        rospy.loginfo(rospy.get_caller_id() + 'Initialising {} node'.format(node_name))
+
+        # Get all parameters from config (rosparam)
+        address = int(rospy.get_param('imu/address', MPU9150_I2C_ADDR_DEFAULT))
+        gyro_scale = int(rospy.get_param('imu/gyroscope/scale', 250))
+        acc_scale = int(rospy.get_param('imu/accelerometer/scale', 2))
+
+        self.imu = Mpu9150(address, gyro_scale, acc_scale)
+
+    def publish(self):
+        queue_size = rospy.get_param('imu/queue', 1)
+        self.pub = rospy.Publisher('imu_readings', Imu, queue_size=queue_size)
+        self.rate = rospy.Rate(rospy.get_param('imu/rate', 10))
+
+        while not rospy.is_shutdown():
+            rospy.loginfo(rospy.get_caller_id() + "publishing IMU data")
+
+            msg = self.pack_message()
+            self.pub.publish(msg)
+
+            self.rate.sleep()
+
+    def pack_message(self):
+        msg = Imu()
+
+        msg.header.stamp = rospy.get_time()
+        msg.header.frame_id = "robocape"
+
+        msg.linear_acceleration.x = self.imu.accel[0]
+        msg.linear_acceleration.y = self.imu.accel[1]
+        msg.linear_acceleration.z = self.imu.accel[2]
+        msg.linear_acceleration_covariance = 0.01 * (1.0, 0.0, 0.0,
+                                                     0.0, 1.0, 0.0,
+                                                     0.0, 0.0, 1.0)
+
+        msg.angular_velocity.x = self.imu.gyro[0]
+        msg.angular_velocity.y = self.imu.gyro[1]
+        msg.angular_velocity.z = self.imu.gyro[2]
+        msg.angular_velocity_covariance = (1.0, 0.0, 0.0,
+                                           0.0, 1.0, 0.0,
+                                           0.0, 0.0, 1.0)
+
+        quaternion = tf.transformations.quaternion_from_euler(self.imu.compass[0],
+                                                              self.imu.compass[1],
+                                                              self.imu.compass[2])
+        msg.orientation.x = quaternion[0]
+        msg.orientation.y = quaternion[1]
+        msg.orientation.z = quaternion[2]
+        msg.orientation.w = quaternion[3]
+        msg.orientation_covariance = 10 * (1.0, 0.0, 0.0,
+                                           0.0, 1.0, 0.0,
+                                           0.0, 0.0, 1.0)
+
+        return msg
+
+if __name__ == '__main__':
+    try:
+        imu = ImuHandler()
+        imu.publish()
+    except rospy.ROSInterruptException:
+        pass
