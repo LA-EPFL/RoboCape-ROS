600b736c97940bb1e362e4a9dbce662bb84e7ad9
Mathieu Bresciani <mathieu.bresciani@epfl.ch>
2015-11-06 15:10:24 +0000

    imu_pub node now publishes magnetometer readings through sensor_msgs/MagneticField

--- robocape_imu/scripts/imu_pub.py
+++ robocape_imu/scripts/imu_pub.py
@@ -3,89 +3,95 @@ from __future__ import division
 import rospy
 import tf
 from sensor_msgs.msg import Imu
+from sensor_msgs.msg import MagneticField
 from mpu9150_driver import Mpu9150
 
 
 class ImuHandler:
     def __init__(self):
-        node_name = 'imu_mpu9150_handler'
-        rospy.init_node(node_name)
-        rospy.loginfo(rospy.get_caller_id() + 'Initialising {} node'.format(node_name))
+        node_name = 'imu_mpu9150_handler';
+        rospy.init_node(node_name);
+        rospy.loginfo(rospy.get_caller_id() + 'Initialising {} node'.format(node_name));
 
         # Get all parameters from config (rosparam)
-        address = int(rospy.get_param('imu/address', 0))
-        gyro_scale = int(rospy.get_param('imu/gyroscope/scale', 250))
-        acc_scale = int(rospy.get_param('imu/accelerometer/scale', 2))
+        address = int(rospy.get_param('imu/address', 0));
+        gyro_scale = int(rospy.get_param('imu/gyroscope/scale', 250));
+        acc_scale = int(rospy.get_param('imu/accelerometer/scale', 2));
 
-        self.imu = Mpu9150(address, gyro_scale, acc_scale)
+        self.imu = Mpu9150(address, gyro_scale, acc_scale);
         self.imu.calibrate();
         rospy.loginfo("Compass calibration; move the imu in random positions during the calibration");
         (self.mag_offset, self.mag_scale) = self.imu.magCalibration();
         rospy.loginfo("Calibration done");
 
     def publish(self):
-        queue_size = rospy.get_param('imu/queue', 1)
-        self.pub = rospy.Publisher('imu_readings', Imu, queue_size=queue_size)
-        self.rate = rospy.Rate(rospy.get_param('imu/rate', 10))
+        queue_size = rospy.get_param('imu/queue', 1);
+        self.pub_imu = rospy.Publisher('imu_readings', Imu, queue_size = queue_size);
+        self.pub_mag = rospy.Publisher('mag_readings', MagneticField, queue_size = queue_size);
+        self.rate = rospy.Rate(rospy.get_param('imu/rate', 10));
 
-        self.msg = self.msg_template()
+        (self.msg_imu, self.msg_mag) = self.msg_template();
 
         while not rospy.is_shutdown():
-            rospy.loginfo(rospy.get_caller_id() + "publishing IMU data")
+            rospy.loginfo(rospy.get_caller_id() + "publishing IMU data");
 
-            self.imu.update()
-            self.pack_message()
-            self.pub.publish(self.msg)
+            self.imu.update();
+            self.pack_message();
+            self.pub_imu.publish(self.msg_imu);
+            self.pub_mag.publish(self.msg_mag);
 
-            self.rate.sleep()
+            self.rate.sleep();
 
     def msg_template(self):
-        msg = Imu()
-        msg.header.frame_id = "robocape"
+        msg_imu = Imu();
+        msg_mag = MagneticField();
+        msg_imu.header.frame_id = "robocape";
+        msg_mag.header.frame_id = "robocape_mag";
 
-        msg.linear_acceleration_covariance = (0.1, 0.0, 0.0,
+        msg_imu.linear_acceleration_covariance = (0.1, 0.0, 0.0,
                                               0.0, 0.1, 0.0,
-                                              0.0, 0.0, 0.1)
+                                              0.0, 0.0, 0.1);
 
-        msg.angular_velocity_covariance = (1.0, 0.0, 0.0,
+        msg_imu.angular_velocity_covariance = (1.0, 0.0, 0.0,
                                            0.0, 1.0, 0.0,
-                                           0.0, 0.0, 1.0)
+                                           0.0, 0.0, 1.0);
 
-        msg.orientation_covariance = (10.0, 0.0,  0.0,
-                                      0.0,  10.0, 0.0,
-                                      0.0,  0.0,  10.0)
+        msg_imu.orientation_covariance = (-1.0, 0.0,  0.0,
+                                      0.0,  0.0, 0.0,
+                                      0.0,  0.0,  0.0);
 
-        return msg
+        msg_mag.magnetic_field_covariance = (10.0, 0.0, 0.0,
+                                        0.0, 10.0, 0.0,
+                                        0.0, 0.0, 10.0);
+        return (msg_imu, msg_mag);
 
     def pack_message(self):
-        now = rospy.get_rostime()
-        self.msg.header.stamp.secs = now.secs
-        self.msg.header.stamp.nsecs = now.nsecs
+        now = rospy.get_rostime();
+        self.msg_imu.header.stamp.secs = now.secs;
+        self.msg_imu.header.stamp.nsecs = now.nsecs;
+        self.msg_mag.header.stamp.secs = now.secs;
+        self.msg_mag.header.stamp.nsecs = now.nsecs;
 
-        #self.msg.linear_acceleration.x = self.imu.accel[0]
-        #self.msg.linear_acceleration.y = self.imu.accel[1]
-        #self.msg.linear_acceleration.z = self.imu.accel[2]
+        self.msg_imu.linear_acceleration.x = self.imu.accel[0];
+        self.msg_imu.linear_acceleration.y = self.imu.accel[1];
+        self.msg_imu.linear_acceleration.z = self.imu.accel[2];
 
-        self.msg.linear_acceleration.x = self.imu.compass[0] - self.mag_offset[0];
-        self.msg.linear_acceleration.y = self.imu.compass[1] - self.mag_offset[1];
-        self.msg.linear_acceleration.z = self.imu.compass[2] - self.mag_offset[2];
+        self.msg_imu.angular_velocity.x = self.imu.gyro[0];
+        self.msg_imu.angular_velocity.y = self.imu.gyro[1];
+        self.msg_imu.angular_velocity.z = self.imu.gyro[2];
 
-        self.msg.angular_velocity.x = self.imu.gyro[0]
-        self.msg.angular_velocity.y = self.imu.gyro[1]
-        self.msg.angular_velocity.z = self.imu.gyro[2]
+        self.msg_imu.orientation.x = 0;
+        self.msg_imu.orientation.y = 0;
+        self.msg_imu.orientation.z = 0;
+        self.msg_imu.orientation.w = 0;
 
-        quaternion = tf.transformations.quaternion_from_euler(self.imu.compass[0],
-                                                              self.imu.compass[1],
-                                                              self.imu.compass[2])
-        self.msg.orientation.x = quaternion[0]
-        self.msg.orientation.y = quaternion[1]
-        self.msg.orientation.z = quaternion[2]
-        self.msg.orientation.w = quaternion[3]
-
+        self.msg_mag.magnetic_field.x = self.imu.compass[0] - self.mag_offset[0];
+        self.msg_mag.magnetic_field.y = self.imu.compass[1] - self.mag_offset[1];
+        self.msg_mag.magnetic_field.z = self.imu.compass[2] - self.mag_offset[2];
 
 if __name__ == '__main__':
     try:
-        imu = ImuHandler()
-        imu.publish()
+        imu = ImuHandler();
+        imu.publish();
     except rospy.ROSInterruptException:
         pass
