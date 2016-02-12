86eefada94a154eb2615cd1487bb734c0f775584
Mathieu Bresciani <mathieu.brescini@epfl.ch>
2016-02-12 13:36:54 +0000

    Add configure.py for GNSS configuration (5Hz). Adjust PPM values for steering servo

--- robocape_ublox/scripts/configure.py
+++ robocape_ublox/scripts/configure.py
@@ -4,9 +4,9 @@ import rospy
 import pyUblox.ublox
 
 def conf():
-   dev = pyUblox.ublox.UBlox('/dev/ttyO4', baudrate=9600, timeout=2)
-   dev.configure_solution_rate(rate_ms=100); # Uptade rate to F=1/100ms = 10Hz
-   dev.set_preferred_dynamic_model(4);       # Set to automotive
+   dev = pyUblox.ublox.UBlox('/dev/ttyO4', baudrate=203400, timeout=2)
+   dev.configure_solution_rate(rate_ms=200); # Uptade rate to F=1/200ms = 5Hz
+   dev.set_preferred_dynamic_model(8);       # Set to airborne with < 4g acceleration (almost no filtering)
    dev.close();
 
 if __name__ == '__main__':
